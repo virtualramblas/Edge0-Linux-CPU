86953f937e5ab32ad271e8dfc2fa821bab959285
virtualramblas <guglielmo.iozzia@gmail.com>
2026-09-19 16:43:37 +0100

    feat(cpu): add Linux CPU reference primitives

---
+++ src/edge0/__init__.py
@@ -0,0 +1,3 @@
+"""Edge0 Linux CPU reference implementation."""
+from .cpu import BailingConfig, group_router, load_safetensors_weights
+__all__ = ["BailingConfig", "group_router", "load_safetensors_weights"]
